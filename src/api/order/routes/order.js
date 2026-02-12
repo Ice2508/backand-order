061108daf448cd0af1a83ba4d263cbe81e2ddccb
Ice2508 <Ice2508@yandex.ru>
2026-02-12 13:32:03 +0300

    ok

--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -6,4 +6,17 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::order.order');
+// Берём стандартные CRUD роуты
+const coreRouter = createCoreRouter('api::order.order');
+
+// Добавляем кастомный роут для оплаты
+coreRouter.routes.push({
+  method: 'POST',
+  path: '/order/pay',
+  handler: 'order.createPayment',
+  config: {
+    auth: false // Public доступ
+  }
+});
+
+module.exports = coreRouter;
